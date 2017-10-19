61ee48682d46e4b18bfb8ddfd915c6cf6f6ba795
Luis Sanchez <luisg@MacBook-Pro-de-Luis.local>
2017-10-19 16:29:08 -0500

    este es mi primer comit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+const lodash = require('lodash')
+//COn esto ya puedes usar lodash
+
+
+
+const obj ={
+  nombre: 'luis',
+  edad: 20,
+  emali: '84luisgilberto@gmail.com',
+  tw: '@luisg_84'
+}
+
+
+
+console.log(obj.edad)
+
+
+/*
+function hola(){
+   return 'Hola mundo 😋'
+}
+
+
+console.log(hola())
+
+
+console.log(2+2)
+var lista = [1,2,3,4,5]
+console.log(lista)
+
+//java script jason
+*/
